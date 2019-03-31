4f8a9f10b62d786380ee705e67aac12eef5c2f8a
xeranimus <nobodycaresaboutspam@gmail.com>
2019-03-31 12:09:27 -0400

    added stable mongodb deployment

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
   db: {
-    uri: 'mongodb://admin:password123@ds117806.mlab.com:17806/essence-events-test', //place the URI of your mongo database here.
+    uri: process.env.PORT ? 'mongodb://admin:password123@ds119755.mlab.com:19755/essence-events-stable' : 'mongodb://admin:password123@ds117806.mlab.com:17806/essence-events-test', //place the URI of your mongo database here.
   },  
   port: process.env.PORT ? process.env.PORT : 8080
 };
