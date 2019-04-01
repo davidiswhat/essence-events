d02a8cdb473e78132553ff77fb89a0a3d0a6de75
Nicholas <namiller@ufl.edu>
2019-03-31 20:47:18 -0400

    set up sign in, log in, and check for sign in status on every page. also added flawed bottom bar to all pages

--- client/js/factories/accountFactory.js
+++ client/js/factories/accountFactory.js
@@ -1,8 +1,19 @@
 angular.module('accounts', []).factory('Accounts', function($http) {
   var methods = {
-	createAccount: function(newAccount) {
+	  createAccount: function(newAccount) {
     console.log("test2");
 	  return $http.post('/api/authenticate', newAccount);
+    },
+
+    logIn: function(Account) {
+      console.log("test4");
+      
+      return $http.post('/api/authenticate', Account);
+    },
+
+    checkLogIn: function() {
+
+      return $http.get('/api/authenticate/status');
     }
   };
 
